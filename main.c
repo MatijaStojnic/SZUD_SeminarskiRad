7a3aa67b522556c37a65dda0077fb2d6120517d3
MatijaStojnic <121751093+MatijaStojnic@users.noreply.github.com>
2024-12-13 21:37:18 +0100

    Dodavanje pocetnog koda u main.c

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int niz[50],n;
+    printf("=================================\n");
+    printf("Pocetna verzija projekta.\n");
+    printf("=================================\n");
+    return 0;
+}
 
